f099e547c80e080a110fd5b4ee72598391b9d3b6
Rémi Boutteau <remi.boutteau@univ-rouen.fr>
2026-02-04 10:33:49 +0100

    maj de l'année du copyritght grace à un script js

---
+++ js/currentYear.js
@@ -0,0 +1,7 @@
+// Mettre à jour l'année en cours dans le copyright
+document.addEventListener('DOMContentLoaded', function() {
+    const yearElement = document.getElementById('currentYear');
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+});
